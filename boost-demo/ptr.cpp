feffddfb3d95e7c0d0c8d031d1ef47c698d2bcea
rabinhu <hulizhong@yeah.net>
2017-06-09 10:28:40 +0800

    Add boost lib.

---
+++ boost-demo/ptr.cpp
@@ -0,0 +1,35 @@
+/*************************************************************************
+	> File Name: ptr.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Tuesday, June 06, 2017 PM05:33:37 HKT
+ ************************************************************************/
+
+#include <iostream>
+#include <boost/scoped_ptr.hpp>
+using namespace std;
+
+
+class FF
+{
+public:
+    int num = 1;
+};
+
+
+int main()
+{
+    FF *f = NULL;
+    {
+        boost::scoped_ptr<FF> ptr(new FF);
+        ptr->num = 11;
+
+        std::cout << (f = ptr.get()) << std::endl;
+        std::cout << ptr.get() << std::endl;
+        std::cout << f << std::endl;
+    }
+    std::cout << sizeof(*f) << std::endl;
+    std::cout << f->num << std::endl;
+    
+    return 0;
+}
